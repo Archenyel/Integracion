5c605e3a9c85c38ed5980e7b063c375cf4fa8402
Angel Manuel Hernández Fragoso <2022371003@uteq.edu.mx>
2025-11-16 13:44:29 -0600

    prueba 2

--- app/index.js
+++ app/index.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
     message: "Despliegue Blue-Green Exitoso",
     host: process.env.HOSTNAME,
     color: process.env.COLOR || "unknown",
-    version: "1.0.0"
+    version: "2.0.0"
   });
 });
 
